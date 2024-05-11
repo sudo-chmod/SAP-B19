5b7a57954e96111c597b23521fb9cbc40c2803b0
chamodh <chamodhhenderson@gmail.com>
2024-05-11 22:06:10 +0530

    Update front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState("First");
+  const [index, setIndex] = useState("");
+  const [nic, setNic] = useState("");
+  const [student, setStudent] = useState("");
+  const [error, setError] = useState("");
+
+  const sendData = (e) => {
+    e.preventDefault();
+    
+    axios.post("http://localhost:6021/api/student/", { index, nic })
+      .then((response) => {
+        if (response.data.code === 200) {
+          setState("Normal");
+          setStudent(response.data.student);
+        } else if (response.data.code === 404) {
+          setState("Incorrect Index");
+          setError("Invalid INDEX! Please enter a valid index number!");
+        } else if (response.data.code === 401) {
+          setState("Incorrect NIC");
+          setError("Incorrect NIC! Please enter the correct NIC!");
+        } else {
+          setState("Error");
+          setError("Oops! Something went wrong!");
+        }
+      })
+      .catch((err) => {
+        setState("Error");
+        setError("Oops! Something went wrong!");
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {state === "First" && (
+        <div>
+          {/* Create the Login Page */}
+        </div>
+      )}
+      {state === "Normal" && (
+        <div>
+          {/* Create the Detail View Page */}
+        </div>
+      )}
+      {state === "Incorrect Index" && (
+        <div>
+          {/* The Login Page with Error */}
+        </div>
+      )}
+      {state === "Incorrect NIC" && (
+        <div>
+          {/* The Login Page with Error */}
+        </div>
+      )}
+      {state === "Error" && (
+        <div>
+          {/* Create Something Went Wrong Error Page */}
+        </div>
+      )}
     </div>
   );
 }
