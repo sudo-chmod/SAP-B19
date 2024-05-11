170337b75b90bb924f7ac3bf5833bca94772b2f1
Chamodh Henderson <87263237+sudo-chmod@users.noreply.github.com>
2024-05-11 22:23:40 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -9,8 +9,12 @@ const app = express()
 const PORT = process.env.PORT
 const DB_URL = process.env.DB_URL
 
-app.use(cors())
 app.use(express.json())
+app.use(cors(
+    {
+        origin: [process.env.FRONT_END_URL]
+    }
+))
 
 app.use('/api/student', studentRoutes)
 app.use('*', (req, res) => {
