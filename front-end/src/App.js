660717b1edf2ff461ef9c3a479f30bfb16d8cd1a
IsuriCS <isurics2@gmail.com>
2024-05-11 23:58:09 +0530

    Login view

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios';
-
+const state = "First";
 function App() {
   const [state, setState] = useState("First");
   const [index, setIndex] = useState("");
@@ -33,34 +33,64 @@ function App() {
       });
   };
 
+  
+
   return (
-    <div>
-      {state === "First" && (
-        <div>
-          {/* Create the Login Page */}
-        </div>
-      )}
-      {state === "Normal" && (
-        <div>
-          {/* Create the Detail View Page */}
-        </div>
-      )}
-      {state === "Incorrect Index" && (
-        <div>
-          {/* The Login Page with Error */}
-        </div>
-      )}
-      {state === "Incorrect NIC" && (
-        <div>
-          {/* The Login Page with Error */}
-        </div>
-      )}
-      {state === "Error" && (
-        <div>
-          {/* Create Something Went Wrong Error Page */}
-        </div>
-      )}
-    </div>
+    <>
+      <div>
+        {state === "First" && (
+          <div>
+            <div className="container" >
+              <div className="row align-items-center" style={{ height: '100vh' }}>
+                <div className="col-12">
+                  <div className="alert alert-light" role="alert">
+                    {/* <div style={{ textAlign: 'center' }}>
+                      <img src="ucsc.png" style={{ width: '150px' }} alt="UCSC logo" />
+                    </div> */}
+                    <br />
+                    <h2 className="alert-heading" style={{ textAlign: 'center' }}>Student Academic Performance-Batch 19</h2>
+                    <br />
+                    <main className="form-signin m-auto col-lg-4">
+                      <form onSubmit={sendData}>
+                        <div className="form-floating">
+                          <input className="form-control" type="text" value={index} onChange={(e) => setIndex(e.target.value)} id="no" name="no" />
+                          <label htmlFor="no">Index No</label>
+                        </div>
+                        <div className="form-floating">
+                          <input className="form-control" type="password" value={nic} onChange={(e) => setNic(e.target.value)} id="pw" name="pw" />
+                          <label htmlFor="pw">NIC</label>
+                        </div>
+                        <button className="btn btn-primary w-100 py-2" type="submit" style={{ marginTop: '2vh' }}>Check Results</button>
+                      </form>
+                    </main>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {state === "Normal" && (
+          <div>
+            {/* Create the Detail View Page */}
+          </div>
+        )}
+        {state === "Incorrect Index" && (
+          <div>
+            {/* The Login Page with Error */}
+          </div>
+        )}
+        {state === "Incorrect NIC" && (
+          <div>
+            {/* The Login Page with Error */}
+          </div>
+        )}
+        {state === "Error" && (
+          <div>
+            {/* Create Something Went Wrong Error Page */}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
