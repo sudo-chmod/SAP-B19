34f8ba765deedfd2513575d65fd83f02f6e2c7e2
IsuriCS <isurics2@gmail.com>
2024-05-12 16:29:41 +0530

    Result View

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -82,7 +82,7 @@ function App() {
                           <input className="form-control" type="text" value={index} onChange={(e) => setIndex(e.target.value)} id="no" name="no" />
                           <label htmlFor="no">Index No</label>
                         </div>
-                        <div className="form-floating">
+                        <div className="form-floating" style={{ marginTop: '2vh' }}>
                           <input className="form-control" type="password" value={nic} onChange={(e) => setNic(e.target.value)} id="pw" name="pw" />
                           <label htmlFor="pw">NIC</label>
                         </div>
@@ -96,11 +96,77 @@ function App() {
           </div>
         )}
         {state === "Normal" && (
-          <div>
-            <p>ojk</p>
-            {/* Create the Detail View Page */}
+          <div className="container">
+            <div className="row" style={{ marginTop: '3vh' }}>
+              <div className="col-12">
+                <h2 className="text-center">Student Academic Performance-Batch 19</h2>
+              </div>
+            </div>
+
+            <p className="text-center">
+              <strong>MC</strong> - Medical | <strong>EC</strong> - Exceptional Circumstances | <strong>CM</strong> - Completed | <strong>NC</strong> - Not Completed | <strong>F</strong> - Absent | <strong>CN</strong> - Cancelled | <strong>WH</strong> - Withheld
+            </p>
+
+            <div className="row">
+              <div className="col-md-6">
+                <h6><strong>Name:</strong> {student.name}</h6>
+                <h6><strong>Index No.: </strong> {student.index}</h6>
+                <h6><strong>Degree Program: </strong> {student.degreeProgram}</h6>
+              </div>
+              <div className="col-md-6">
+                <h6><strong>Current GPA: </strong> {student.gpa}</h6>
+                <h6><strong>Current Rank: </strong> {student.rank}</h6>
+                <h6><strong>Total Credits: </strong> {student.totalCredit}</h6>
+              </div>
+            </div>
+
+            <div className="row" style={{ marginTop: '3vh' }}>
+              <div className="col-12">
+                <h2 className="text-left">Results</h2>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-12">
+                <div className="table-responsive">
+                  <table className="table table-bordered table-sm">
+                    <thead >
+                      <tr>
+                        <th style={{ background: "beige" }}>Subject Code</th>
+                        <th style={{ background: "beige" }}>Subject Name</th>
+                        <th style={{ background: "beige" }}>Grade</th>
+                        <th style={{ background: "beige" }}>Credit</th>
+                        <th style={{ background: "beige" }}>Credit x Grade</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {student.result.map((result, index) => (
+                        <tr key={index}>
+                          <td>{result.subCode}</td>
+                          <td>{result.subName}</td>
+                          <td>{result.subGrade}</td>
+                          <td>{result.subCredit}</td>
+                          <td>{result.subCreditXGrade}</td>
+                        </tr>
+                      ))}
+                      <tr>
+                        <td colSpan="3"></td>
+                        <td><strong>Total Credits:{student.totalCredit} </strong></td>
+                        <td><strong>Total: {student.totalCreditXGrade}</strong></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <p style={{ marginLeft: '2vw' }}>
+                  <strong>GPA = &#931; (Grade x Credit ) / Total Credit</strong><br />
+                  &nbsp; &nbsp; &nbsp; &nbsp; = {student.totalCreditXGrade} / {student.totalCredit}<br />
+                  &nbsp; &nbsp; &nbsp; &nbsp; = {student.gpa}
+                </p>
+              </div>
+            </div>
           </div>
         )}
+
         {state === "Error" && (
           // need to change again
           <div>
